e71f4d2a992935d9a6ac0bef3d3fbda9cd274c34
AntoniWalburg <antek.walburg1989spotify@gmail.com>
2022-04-08 19:44:14 +0200

    Updated files

--- DiagonalDifference/diagonalDifference.py
+++ DiagonalDifference/diagonalDifference.py
@@ -4,23 +4,16 @@ import random
 import re
 import sys
 
-
 def diagonalDifference(arr):
     return abs(sum(arr[x][x] for x in range(n)) - sum(arr[x][n - x - 1] for x in range(n)))
 
-
 if __name__ == '__main__':
+
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
-
     arr = []
-
     for _ in range(n):
         arr.append(list(map(int, input().rstrip().split())))
-
     result = diagonalDifference(arr)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
